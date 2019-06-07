47b9e2b46ab7f34514b6acb507b2014403088fa0
Simon Lally <simonlallydev@gmail.com>
2019-06-06 21:24:12 -0400

    added styling to buttons and input fields

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,86 +1,80 @@
 <template>
   <div id="app">
-    <h1>Combinations</h1>
-    <form class='input-form'>
+    <div class="main">
+      <h1>Coin Combinations</h1>
+      <form class="input-form">
         <ul>
-            <li v-for="(item, index) in items" :key="index" >
-                <div>
-                    <input type="text" placeholder="Name" ref="name" v-model="item.name">  
-                    <input type="number" placeholder="Denomination" ref="denom" v-model="item.denom">
-                </div>
-            </li>
+          <li v-for="(item, index) in items" :key="index">
+            <div>
+              <input type="text" placeholder="Name" ref="name" v-model="item.name">
+              <input type="number" placeholder="Denomination" ref="denom" v-model="item.denom">
+            </div>
+          </li>
         </ul>
         <button v-on:click="addAnotherDenom($event)">Add another denomination</button>
         <button v-on:click="printCombinations($event)">Print Combinations</button>
-    </form>
+      </form>
+      <div>
+        <br>
+        <br>
+        <br>
+        <table v-for="(item, index) in result" :key="index">
+          <tr>
+            <span v-for="(x, index) in items" :key="index"> {{x.name}} </span>
+          </tr>
 
-    <div>         
-      <br>
-      <br>
-      <br>        
-      <table v-for="(item, index) in result" :key="index">
-        <tr>
-          <span v-for="(x, index) in items" :key="index">
-            {{x.name}}
-          </span>
-        </tr>
-        <tr v-for="(x, index) in item" :key="index">
-          <span v-for="(y, index) in x" :key="index">
-            <span v-if="y>=0">{{y}}</span>
-          </span> 
-        </tr>  
-          <p>The count is : {{item.length}}</p> 
-      </table>
+          <tr v-for="(x, index) in item" :key="index">
+            <span v-for="(y, index) in x" :key="index">
+              <td v-if="y>=0">
+                <th> {{y}} </th>
+              </td>
+            </span>
+          </tr>
+        </table>
+
       </div>
+    </div>
   </div>
 </template>
 
+<style lang="css">
+@import "style.css";
+</style>
+
 <script>
 export default {
-  data: function () {
+  data: function() {
     return {
-      items: [
-        {name: "", denom: 0},
-        {name: "", denom: 0}
-      ],
+      items: [{ name: "", denom: 0 }, { name: "", denom: 0 }],
       message: "message",
       result: []
-    }
+    };
   },
   methods: {
-      printCombinations: function (event) {
-        var vm = this;
-        console.log("DOES THIS WORK");
-        console.log(this.items);
-        event.preventDefault();
+    printCombinations: function(event) {
+      var vm = this;
+      event.preventDefault();
 
-        $.ajax({
-          type: "POST",
-          url: '/api/coin',
-          data: {items: this.items},
-          success: function(data) {
-            // on success set the response to result
-            vm.result.push(data);
-          }
-        });
+      this.result = [];
 
-
-      },
-      addAnotherDenom: function(event) {
-        event.preventDefault();
-        var newItem = {name: "", denom: 0};
-        this.items.push(newItem);
-      }
+      $.ajax({
+        type: "POST",
+        url: "/api/coin",
+        data: { items: this.items },
+        success: function(data) {
+          // on success set the response to result
+          vm.result.push(data);
+        }
+      });
+    },
+    addAnotherDenom: function(event) {
+      event.preventDefault();
+      var newItem = { name: "", denom: 0 };
+      this.items.push(newItem);
     }
-    
-}
+  }
+};
 </script>
 
-<style scoped>
-p {
-  font-size: 2em;
-  text-align: center;
-}
-</style>
 
 
